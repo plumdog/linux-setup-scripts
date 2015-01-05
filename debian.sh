76853d967df8d3ec5091b0969a35a6d36f02fd4f
Andrew Plummer <plummer574@gmail.com>
2015-01-05 09:32:56 +0000

    Fix recompile of xmonad

--- debian.sh
+++ debian.sh
@@ -9,7 +9,7 @@ sudo apt-get upgrade
 
 # install some packages
 sudo apt-get install xmonad zsh emacs vcsh feh gmrun slock keepassx xmobar
-xmonad --recompile
+
 
 # change to use zshell
 sudo chsh -s /usr/bin/zsh andrew
@@ -20,3 +20,6 @@ vcsh clone https://github.com/plumdog/dotzshrc zshrc
 vcsh clone https://github.com/plumdog/dotgit git
 vcsh clone https://github.com/plumdog/dotxmonad xmonad
 vcsh clone https://github.com/plumdog/dotxinitrc xinitrc
+
+# recompile xmonad setup with new dotfiles
+xmonad --recompile
