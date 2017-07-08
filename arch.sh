c9bc3a072b1e41ea22a8bdf0011d769790ea6a22
Andrew Plummer <plummer574@gmail.com>
2017-07-08 16:23:40 +0100

    Add arch script

---
+++ arch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# install some packages
+sudo pacman -Syu \
+    xmonad \
+    xmonad-contrib \
+    zsh \
+    emacs \
+    feh \
+    dmenu \
+    slock \
+    keepassx \
+    keepass \
+    xmobar \
+    wmctrl \
+    wget \
+    openssh
+
+bash <(curl aur.sh) -si \
+    vcsh \
+    google-chrome \
+    dropbox \
+    silver-searcher-git
+
+sudo ln -s /usr/bin/google-chrome-stable /usr/bin/google-chrome
+
+# ... openssh-server? ag?
+
+sudo systemctl enable dhcpcd
+sudo systemctl enable dropbox@$USER
+
+# change to use zshell
+sudo chsh -s /usr/bin/zsh $USER
+
+# install my dotfiles with vcsh so things feel homely
+vcsh clone https://github.com/plumdog/dotemacs emacs
+vcsh clone https://github.com/plumdog/dotzshrc zshrc
+vcsh clone https://github.com/plumdog/dotgit git
+vcsh clone https://github.com/plumdog/dotxmonad xmonad
+vcsh clone https://github.com/plumdog/dotxinitrc xinitrc
+
+# recompile xmonad setup with new dotfiles
+xmonad --recompile
